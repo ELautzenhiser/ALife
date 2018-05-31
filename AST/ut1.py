ddd307284031212ffad5ffb1c8fcd507e3ddfc73
Benjamin Jones <BenjaminPaikJones@gmail.com>
2018-05-31 09:13:56 -0400

    Fixed the bug in txt2src caused by strings containing {} that should not be replaced with getTree(). (#14)
    * Create AST generator V1
        - Can generate the AST .txt file for programs using a subset of the
          python language
        - This subset includes functions, binary operations, print statements,
          variable assignments and for loopsw
    
    * Removed .pyc file.
    
    * Add imports and globals, and add parentheses to BinOps to ensure order of operations.
    
    * Fix bugs related to quotes in strings. Add functionality such as object attributes and subscripts.
    
    * Update While loops: body is no longer comma delimited, and else clauses can now be used.
    
    * ut1 (unit test 1) can "pretty print" ast trees.  This will help both for human reading as well as diffing two ast trees.
    
    * Cwqonvert txt back to source (txt2src is basically genomeGenerator.translate, except it doesn't use a codon list)
    
    * Verify that a program can be "round-tripped".
        - ut1.py cycles from source to an ast object to {}-formatted ast, bakc to source, and finally to another ast object
        - It then diffs the two ast.trees to guarantee ast2txt translated correctly.
        - BUG: this test crashes if basically anything goes wrong.
    
    * Fix crashes due to a failure of getTree, and diff correctly.
    
    * Enable curly braces in quotes.
    
    * Fix bug in txt2src caused by strings including curly brackets.
    
    * Complete pangram (for existing constructs).
    
    * Do not allow ast2txt to be called on lists.
        - The reasoning behind this is that lists are not always handled in the
          same way.
        - For example, For.body is a list and should be represented as
          statements separated by newlines
        - On the other hand, Tuple.elts is a list, but its elements should be
          separated by commas
        - Compare() also has lists that are handled in a completely different way
        - For safety's sake, it is better to get a loud warning than a silent bug
    
    * Change indent from 5 spaces to 4 in the else clause of genomeGenerator.translate() and regenerate 000_aaaaaaaa.py.
    
    * ast2txt now translates all functionality in 000_aaaaaaaa.py.

--- AST/ut1.py
+++ AST/ut1.py
@@ -37,13 +37,17 @@ def getTree(idx, split_txt):
     aacid = split_txt[idx]
     idx += 1
     subtrees = []
-    for i in xrange(aacid.count('{}')):
-        try:
-            subtree, idx = getTree(idx, split_txt)
-        except:
-            subtree, idx = "ERRORED subtree", idx + 1
+    num_subtrees = aacid.replace('{{', "").replace('}}', "").count('{}')
+    for i in xrange(num_subtrees):
+        subtree, idx = getTree(idx, split_txt)
         subtrees.append(subtree)
-    aacid = aacid.format(*subtrees)
+    try:
+        aacid = aacid.format(*subtrees)
+    except Exception as error:
+        print error
+        print "aacid:", aacid, "-- subtrees:", subtrees
+        aacid = "# txt2src could not parse here.\n"
+        
     return aacid, idx
 
 def txt2src(txt_str):
@@ -70,28 +74,43 @@ def txt2src(txt_str):
 if __name__ == '__main__':
     f, filename = getValidFileFromUser()
     source = f.read()
+    f.close()
     tree = ast.parse(source)
 
-    txtTree = ast2txt(tree)
-    print txtTree
+    txt_tree = ast2txt(tree)
+    print(txt_tree)
 
     # round-trip source
-    rt_source = txt2src(txtTree) 
-    print rt_source
+    rt_source = txt2src(txt_tree) 
 
     # Finally, run ast again.
-    rt_tree = ast.parse(rt_source)
-
-    print pdumpAST(tree)
-    print pdumpAST(rt_tree)
+    rt_tree = None
+    try:
+        rt_tree = ast.parse(rt_source)
+    except Exception as error:
+        print "Re-translated source had syntax errors:"
+        print "    ", error
+        failed_ast_name = filename[:-3] + '(failed).txt'
+        f = open(failed_ast_name, 'w+')
+        f.write(txt_tree)
+        f.close()
+        failed_src_name = filename[:-3] + '(failed).py'
+        f = open(failed_src_name, 'w+')
+        f.write(rt_source)
+        f.close()
+        print "Translations written to file:"
+        print "    ", failed_src_name
+        print "    ", failed_ast_name
+        quit()
 
     # diff
     num_lines = 0
     before = pdumpAST(tree).split('\n')
     after = pdumpAST(rt_tree).split('\n')
-    for line in unified_diff(before, after):
+    for line in unified_diff(before, after, 'before', 'after'):
         num_lines += 1
         print line
 
     if not num_lines:
+        print pdumpAST(rt_tree)
         print "TEST PASSED: ASTs are identical!"
