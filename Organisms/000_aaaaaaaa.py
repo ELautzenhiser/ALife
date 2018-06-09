961fd9a540801e9c95d7f6a9dbdbb0f756e04ea3
Ealautzenhiser <Ealautzenhiser@gmail.com>
2018-06-08 22:12:56 -0400

    Add movement based on inpetus
    Movement now takes parameters that control direction and distance moved.

--- Organisms/000_aaaaaaaa.py
+++ Organisms/000_aaaaaaaa.py
@@ -1,6 +1,6 @@
-translation = {'098': '_', '099': '.py', '126': 'motivation', '090': 'range', '091': 'choice', '092': 'makeOffspring', '093': 'location_id', '094': 'unique_id', '095': '8', '096': 'alphanumeric', '097': 'offspring_name', '133': 'organisms', '132': 'initiateInteraction', '012': 'codon_length', '130': '-20', '014': 'world', '015': 'sight_distance', '135': 'interact', '134': 'other_name', '018': '3', '019': '{}[{}]', '139': 'other_organism', '138': 'getMessage', '127': '({} or {})', '120': 'uniqueID', '121': 'new_name', '027': '-1', '123': 'tryToMove', '021': '{}.{}', '020': '{}({})', '023': '0', '022': 'argv', '128': '{} is {}', '129': 'uniform', '029': 'getcwd', '028': '/', '010': 'global {}', '011': 'metabolism', '131': '20', '013': 'self_name', '137': 'message', '136': 'otherName', '016': '{} = {}', '084': 'chars', '017': '0.5', '144': 'stdin', '115': 'distance', '114': 'getNewPosition', '038': 'aacid', '039': 'translation', '111': 'intToString', '110': 'name', '113': '({} - {})', '112': 'num', '032': 'getTree', '033': 'position', '030': '300', '031': 'dedent', '119': 'move', '037': '({} + {})', '034': 'codon', '035': 'genome', '051': 'xrange', '171': 'get', '170': 'type', '108': 'close', '109': 'splitName', '148': 'universal_newlines', '049': 'for {} in {}:', '048': '{{}}', '102': 'w', '046': '}}}}', '045': '{{{{', '044': 'replace', '043': 'num_subacids', '107': '\\"', '104': 'translation = ', '040': '{} += {}', '151': 'communicate', '150': 'full_response', '153': 'dict', '047': 'count', '152': 'parsed_response', '058': 'except{}:', '059': '#', '103': 'write', '054': 'append', '055': 'try:', '056': 'format', '057': '*{}', '050': 'i', '172': 'print {}', '052': '({})', '073': ':', '101': 'open', '157': 'organism', '106': 'genome = \\"', '071': '\\n', '042': '[{}]', '041': 'new_container', '105': 'str', '061': 'translate', '060': 'return {}', '063': 'code_list', '062': 'output', '065': '{} < {}', '064': 'while {}:', '067': 'indent', '066': 'len', '069': 'if {}:', '068': 'line', '168': 'raw_input', '169': 'loads', '164': 'organism_location', '165': 'abs', '166': '{} > {}', '167': 'start_conditions', '160': 'findNearbyOrganisms', '161': 'location', '162': 'nearby', '163': '{} != {}', '117': 'int', '116': '({} % {})', '076': '4', '077': '1', '074': '({} * {})', '075': ' ', '155': 'dumps', '154': 'acting_organism', '070': '{} == {}', '156': 'getAllOrganisms', '159': '-3', '158': 'listdir', '078': '{} -= {}', '079': 'max', '100': 'offspring', '088': '0123456789abcdefghijklmnopqrstuvwxyz', '036': '{}:{}', '072': 'else:', '118': '1000', '053': 'value', '146': 'stdout', '147': 'stderr', '089': 'string', '145': 'PIPE', '142': '{}={}', '143': 'bufsize', '140': 'Popen', '141': 'python', '083': 'letters', '082': 'kind', '081': 'length', '080': 'getRandomString', '087': '0123456789', '086': 'numbers', '085': 'abcdefghijklmnopqrstuvwxyz', '149': 'True', '003': 'random', '002': 'sys', '001': '{}, {}', '000': 'import {}', '007': '', '006': 'subprocess', '005': 'json', '004': 'os', '009': 'getTraits', '008': 'def {}({}):', '025': "'{}'", '024': 'split', '122': 'rename', '026': '\\\\', '124': 'end_point', '125': 'None'}
-genome = '000001002001003001004001005006007008009007010011010012010013010014010015016011017016012018016013019020021019020021019021002022023024025026027024025028027016014020021004029007016015030031007008032033016034019035036033037033012016038019039034040033012016041042007016043020021020021020021038044001025045025007044001025046025007047025048049050020051043016052001053033020032033020021041054053031055016038020021038056057041031058007016038037025059038031060052001038033031007008061007016062025007016063042007016033023064065033020066035016052001053033020032033020021063054053031016067023049068063069070020066068023040062025071031072069070019068027025073040062037037025071074025075074076067068040067077031072069070068025031078067077016067020079001067023031072040062037037025071074025075074076067068031031031031060062031007008080001081082069070082025083016084025085031072069070082025086016084025087031072016084025088031031016089025007049050020090081040089020021003091084031060089031007008092007016093020080001018025086016094020080001095025096016097037037037093025098094025099016100020101001097025102020021100103037025104020105039020021100103037037037025071025106035025107020021100103020061007020021100108007031007008109110060020021110024025098031007008111001112081016112020105112049050020051113081020066112016112037025023112031060112031007008114001033115060116037020117033115118031007008119115010013016052001033120020021013024025098016033020111001020114001033115018016121037037033025098120020021004122001013121016013121031007008123001016124125016126023069127128124125070126023016115020117074020021003129001130131011020119115031031007008132133016134020021003091133020135134031007008135136016137020138007016139020021006140001042001025141136001142143077001142144021006145001142146021006145001142147021006145142148149016150020021139151137016152019150023031007008138007016137020153007016019137025154013060020021005155137031007008156014016133042007049157020021004158014069070019157036159007025099020021133054157031031060133031007008160133016161020117019020109013023016162042007049157133069163157013016164020117019020109157023069127065020165113164161015166020165113164161113118015020021162054157031031031060162031020009007016167020168007055016167020021005169167031058007016167020153007031069163020170167153016167020153007031069128020021167171025154125020123007016133020156014016162020160133069162020132162031020092007031072020123007172020138007031'
-import sys, random, os, json, subprocess
+translation = {'098': 'offspring_name', '099': '_', '023': 'argv', '090': 'string', '091': 'range', '092': 'choice', '093': 'makeOffspring', '094': 'location_id', '095': 'unique_id', '096': '8', '097': 'alphanumeric', '133': 'distance_two', '011': 'global {}', '131': 'distance_one', '013': 'codon_length', '137': '({} and {})', '015': 'world', '016': 'sight_distance', '134': 'abs', '018': '20', '019': '3', '139': '{}{}', '138': '{} > {}', '127': 'motivation', '120': 'move', '024': '0', '122': 'new_name', '123': 'rename', '021': '{}({})', '020': '{}[{}]', '126': 'None', '022': '{}.{}', '128': '{} is {}', '129': 'numpy', '029': '/', '028': '-1', '010': 'getTraits', '132': 'min', '012': 'metabolism', '130': 'normal', '014': 'self_name', '136': 'stimulus_direction', '135': 'shortest_distance', '084': 'letters', '017': '{} = {}', '089': '0123456789abcdefghijklmnopqrstuvwxyz', '115': 'getNewPosition', '054': 'value', '038': '({} + {})', '039': 'aacid', '111': 'name', '110': 'splitName', '113': 'num', '055': 'append', '032': 'dedent', '033': 'getTree', '030': 'getcwd', '031': '300', '036': 'genome', '037': '{}:{}', '034': 'position', '035': 'codon', '057': 'format', '050': 'for {} in {}:', '051': 'i', '052': 'xrange', '053': '({})', '108': '\\"', '109': 'close', '085': 'chars', '049': '{{}}', '048': 'count', '102': 'open', '046': '{{{{', '100': '.py', '044': 'num_subacids', '043': '[{}]', '042': 'new_container', '041': '{} += {}', '105': 'translation = ', '151': 'bufsize', '150': '{}={}', '074': ':', '047': '}}}}', '152': 'stdin', '058': '*{}', '059': 'except{}:', '179': 'type', '178': 'loads', '177': 'raw_input', '176': 'start_conditions', '056': 'try:', '174': 'organism_location', '173': '{} != {}', '045': 'replace', '171': 'location', '170': 'findNearbyOrganisms', '101': 'offspring', '157': 'True', '106': 'str', '175': '({} or {})', '156': 'universal_newlines', '145': 'message', '107': 'genome = \\"', '182': 'other_friendliness', '183': 'float', '180': 'get', '181': 'other_location', '104': 'write', '040': 'translation', '061': 'return {}', '060': '#', '063': 'output', '062': 'translate', '065': 'while {}:', '064': 'code_list', '067': 'len', '066': '{} < {}', '069': 'line', '068': 'indent', '168': 'listdir', '169': '-3', '164': 'friendliness', '165': 'dumps', '166': 'getAllOrganisms', '167': 'organism', '160': 'parsed_response', '161': 'print {}', '162': 'dict', '163': 'acting_organism', '114': '({} - {})', '117': '({} % {})', '116': 'distance', '103': 'w', '076': ' ', '077': '4', '153': 'PIPE', '075': '({} * {})', '072': '\\n', '073': 'else:', '070': 'if {}:', '071': '{} == {}', '159': 'communicate', '158': 'full_response', '112': 'intToString', '078': '1', '079': '{} -= {}', '119': '1000', '118': 'int', '155': 'stderr', '146': 'getMessage', '147': 'other_organism', '144': 'interact', '088': '0123456789', '142': 'organisms', '143': 'other_name', '140': '-shortest_distance', '141': 'initiateInteraction', '083': 'kind', '082': 'length', '081': 'getRandomString', '080': 'max', '087': 'numbers', '086': 'abcdefghijklmnopqrstuvwxyz', '148': 'Popen', '149': 'python', '003': 'random', '002': 'sys', '001': '{}, {}', '000': 'import {}', '007': 'numpy.random', '006': 'subprocess', '005': 'json', '004': 'os', '154': 'stdout', '009': 'def {}({}):', '008': '', '025': 'split', '121': 'uniqueID', '027': '\\\\', '172': 'nearby', '026': "'{}'", '124': 'tryToMove', '125': 'end_point'}
+genome = '000001002001003001004001005001006007008009010008011012011013011014011015011016017012018017013019017014020021022020021022020022002023024025026027028025026029028017015021022004030008017016031032008008009033034017035020036037034038034013017039020040035041034013017042043008017044021022021022021022039045001026046026008045001026047026008048026049050051021052044017053001054034021033034021022042055054032056017039021022039057058042032059008017039038026060039032061053001039034032008008009062008017063026008017064043008017034024065066034021067036017053001054034021033034021022064055054032017068024050069064070071021067069024041063026072032073070071020069028026074041063038038026072075026076075077068069041068078032073070071069026032079068078017068021080001068024032073041063038038026072075026076075077068069032032032032061063032008008009081001082083070071083026084017085026086032073070071083026087017085026088032073017085026089032032017090026008050051021091082041090021022003092085032061090032008008009093008017094021081001019026087017095021081001096026097017098038038038094026099095026100017101021102001098026103021022101104038026105021106040021022101104038038038026072026107036026108021022101104021062008021022101109008032008008009110111061021022111025026099032008008009112001113082017113021106113050051021052114082021067113017113038026024113032061113032008008009115001034116061117038021118034116119032008008009120116011014017053001034121021110014017034021112001021115001034116019017122038038034026099121021022004123001014122017014122032008008009124001017125126017127024070128125126017116021118021022022129003130001024012021120116032073017034021118020021110014024017131038114119021080001125034021132001125034017133021134114125034017135021132001131133070066125034017136028032073017136078032017116021118021022022129003130001075075136127012012070137138127024138021134116135070138116024017116135032073017116139140008032032021120116032032008008009141142017143021022003092142021144143032008008009144143017145021146008017147021022006148001043001026149143001150151078001150152022006153001150154022006153001150155022006153150156157017158021022147159145017160020158024161160032008008009146008017145021162008017020145026163014017020145026164021022022129003130001024078061021022005165145032008008009166015017142043008050167021022004168015070071020167037169008026100021022142055167032032061142032008008009170142017171021118020021110014024017172043008050167142070173167014017174021118020021110167024070175066021134114174171016138021134114174171114119016021022172055167032032032061172032008021010008017176021177008056017176021022005178176032059008017176021162008032070173021179176162017176021162008032070128021022176180026163126021124008017142021166015017172021170142070172021141172032021093008032073017181021118020021110021022176180026163024017182021183021022176180026164021124001181182161021146008032'
+import sys, random, os, json, subprocess, numpy.random
 
 def getTraits():
     global metabolism
@@ -8,11 +8,12 @@ def getTraits():
     global self_name
     global world
     global sight_distance
-    metabolism = 0.5
+    metabolism = 20
     codon_length = 3
     self_name = sys.argv[0].split('\\')[-1].split('/')[-1]
     world = os.getcwd()
     sight_distance = 300
+
 
 def getTree(position):
     codon = genome[position:(position + codon_length)]
@@ -28,6 +29,7 @@ def getTree(position):
     except:
         aacid = ('#' + aacid)
     return (aacid, position)
+
 
 def translate():
     output = ''
@@ -52,6 +54,7 @@ def translate():
                     output += (('\n' + (' ' * (4 * indent))) + line)
     return output
 
+
 def getRandomString(length, kind):
     if kind == 'letters':
         chars = 'abcdefghijklmnopqrstuvwxyz'
@@ -65,6 +68,7 @@ def getRandomString(length, kind):
         string += random.choice(chars)
     return string
 
+
 def makeOffspring():
     location_id = getRandomString(3, 'numbers')
     unique_id = getRandomString(8, 'alphanumeric')
@@ -75,8 +79,10 @@ def makeOffspring():
     offspring.write(translate())
     offspring.close()
 
+
 def splitName(name):
     return name.split('_')
+
 
 def intToString(num, length):
     num = str(num)
@@ -84,36 +90,61 @@ def intToString(num, length):
         num = ('0' + num)
     return num
 
+
 def getNewPosition(position, distance):
     return ((int(position) + distance) % 1000)
 
+
 def move(distance):
     global self_name
-    (position, uniqueID) = self_name.split('_')
+    (position, uniqueID) = splitName(self_name)
     position = intToString(getNewPosition(position, distance), 3)
     new_name = ((position + '_') + uniqueID)
     os.rename(self_name, new_name)
     self_name = new_name
 
+
 def tryToMove(end_point = None, motivation = 0):
-    if (end_point is None or motivation == 0):
-        distance = int((random.uniform(-20, 20) * metabolism))
+    if end_point is None:
+        distance = int(numpy.random.normal(0, metabolism))
         move(distance)
+    else:
+        position = int(splitName(self_name)[0])
+        distance_one = ((1000 - max(end_point, position)) + min(end_point, position))
+        distance_two = abs((end_point - position))
+        shortest_distance = min(distance_one, distance_two)
+        if end_point < position:
+            stimulus_direction = -1
+        else:
+            stimulus_direction = 1
+        distance = int(numpy.random.normal(((stimulus_direction * motivation) * metabolism), metabolism))
+        if (motivation > 0 and abs(distance) > shortest_distance):
+            if distance > 0:
+                distance = shortest_distance
+            else:
+                distance = -shortest_distance
+        move(distance)
+
 
 def initiateInteraction(organisms):
     other_name = random.choice(organisms)
     interact(other_name)
 
-def interact(otherName):
+
+def interact(other_name):
     message = getMessage()
-    other_organism = subprocess.Popen(['python', otherName], bufsize=1, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    other_organism = subprocess.Popen(['python', other_name], bufsize=1, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     full_response = other_organism.communicate(message)
     parsed_response = full_response[0]
+    print parsed_response
+
 
 def getMessage():
     message = dict()
     message['acting_organism'] = self_name
+    message['friendliness'] = numpy.random.normal(0, 1)
     return json.dumps(message)
+
 
 def getAllOrganisms(world):
     organisms = []
@@ -121,6 +152,7 @@ def getAllOrganisms(world):
         if organism[-3:] == '.py':
             organisms.append(organism)
     return organisms
+
 
 def findNearbyOrganisms(organisms):
     location = int(splitName(self_name)[0])
@@ -131,6 +163,7 @@ def findNearbyOrganisms(organisms):
             if (abs((organism_location - location)) < sight_distance or abs((organism_location - location)) > (1000 - sight_distance)):
                 nearby.append(organism)
     return nearby
+
 getTraits()
 start_conditions = raw_input()
 try:
@@ -147,5 +180,7 @@ if start_conditions.get('acting_organism') is None:
         initiateInteraction(nearby)
     makeOffspring()
 else:
-    tryToMove()
+    other_location = int(splitName(start_conditions.get('acting_organism'))[0])
+    other_friendliness = float(start_conditions.get('friendliness'))
+    tryToMove(other_location, other_friendliness)
     print getMessage()
